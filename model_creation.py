dff2912ef5b2be2e97f345aaa94e90fc9d096290
naimurborno <naimurborno@gmail.com>
2024-02-21 13:01:43 +0600

    dags hub is integreted

--- model_creation.py
+++ model_creation.py
@@ -8,31 +8,48 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import ElasticNet
 import mlflow
 import sys
+from sklearn.metrics import mean_absolute_error,r2_score,mean_squared_error
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
 
 if __name__=="__main__":
-    with mlflow.start_run() as run:
-        #reading the cleaned dataset
-        dataset=pd.read_csv("cleaned_data.csv")
-        X=dataset.drop(columns=['Loan_Status'])
-        y=dataset['Loan_Status']
-
-        X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,shuffle=True)
-        # n_estimator=float(sys.argv[1] if len(sys.argv[1]>1 else 10))
-        alpha=float(sys.argv[1] if len(sys.argv[1])>1 else 1)
-        l1=float(sys.argv[2] if len(sys.argv[2])>1 else 0.6)
-        # model=RandomForestClassifier(n_estimators=n_estimator)
-        model=ElasticNet(alpha=alpha,l1_ratio=l1)
+    dataset=pd.read_csv("cleaned_data.csv")
+    X=dataset.drop(columns=["Loan_Status"])
+    y=datset['Loan_Status']
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,shuffle=True)
+    alpha=float(sys.argv[1]) if len(sys.argv[1]>1) else 0.5
+    l1=float(sys.argv[2]) if len(sys.argv[2]>1) else 1
+    with mlflow.start_run():
+        model=ElasticNet(alpha=alpha,l1_ration=l1)
         model.fit(X_train,y_train)
+
+        predicted=model.predict(X_test)
+        (rmse,mae,r2)=eval_metrics(y_test,predicted)
+        print(f"the rmse value is:{rmse}")
+        print(f"the mae value is:{mae}")
+        print(f"the r2 score value is:{r2}")
+        mlflow.log_param("alpha",alpha)
+        mlflow.log_param("l1_ratio,l1")
+        mlflow.log_metric("rmse",rmse)
+        mlflow.log_metric("mae",mae)
+        mlflow.log_metric("r2_score",r2)
 
         remote_server_uri="https://dagshub.com/naimurborno/Loan_prediction_tracking_using_mlflow.mlflow"
         mlflow.set_tracking_uri(remote_server_uri)
-        tracking_url_type_store=urlparse(mlflow.get_tracking_uri()).scheme
 
-        if tracking_url_type_store!="file":
-            mlflow.log_model(model,"model",registered_model_name="ElasticnetWineModel")
-        else:
+        tracking=urlparse(mlflow.get_tracking_uri()).scheme
+        if tracking !="file":
             mlflow.log_model(model,"model")
+        else:
+            mlflow.sklearn.log_model(model,"model")
 
+
+
+    
 
         
 
